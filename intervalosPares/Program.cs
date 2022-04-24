fe44dc124a71c51ed2de14ccd7fb3a0155c397c1
Veiga <roberto.veiga@gmail.com>
2022-04-23 22:18:26 -0300

    intervalosPares

---
+++ intervalosPares/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace intervalosPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(intervaloPares(1,20,4));
+        }
+    
+        static string intervaloPares(int inicio, int fim, int quantidade) 
+        {
+            int quant=0;
+            int[] lista;
+            lista = new int[quantidade];
+            for (int i=inicio; i<=fim && quant < quantidade; i++)
+            {
+                if (i%2 == 0)
+                {
+                    //Console.WriteLine($"i= {i} é par");
+                    lista[quant]=i;
+                    quant +=1;
+
+                }
+            }
+             return String.Join(",",lista);
+        }
+    }
+
+     
+
+}
