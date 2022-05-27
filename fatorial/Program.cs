d11b1407cd1bbdfc15cc3b093cd41947dab34214
Veiga <roberto.veiga@gmail.com>
2022-05-27 13:08:02 -0300

    fatorial

---
+++ fatorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("digite um número inteiro positivo para calcular o fatorial: ");
+            
+            var n = int.Parse( Console.ReadLine());
+            var res = fatorial(n);
+            System.Console.WriteLine(res);
+        }
+
+        static int fatorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            } return (n * fatorial(n-1));
+            
+            
+        }
+    }
+}
